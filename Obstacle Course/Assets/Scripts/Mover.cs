a7f922cf5b0c61420a18d33f5ea5e4bd897b094b
RagingTurtle <tinybit210r@gmail.com>
2022-02-03 01:18:19 -0600

    Add ground, player and mover script.

---
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+                transform.Translate(.01f,0,0);
+
+    }
+}
