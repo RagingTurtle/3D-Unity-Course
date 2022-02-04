7219117bd40954c2f8fab716a784718b0b2dfa9f
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-02-03 20:18:21 -0600

    Variables

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    public float xValue = 0;
+    public float yValue = 0;
+    public float zValue = 0;
+    public float step = 0.01f;
     void Start()
     {
     }
 
     void Update()
     {
-                transform.Translate(.01f,0,0);
-
+                transform.Translate(xValue,yValue,zValue);
+                yValue += step;
     }
 }
