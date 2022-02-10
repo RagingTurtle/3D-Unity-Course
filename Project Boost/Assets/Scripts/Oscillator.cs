5cee200348657e4ab1e3d6a39ab882e6acedc787
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-02-10 12:49:51 -0600

    Added math code for moving obstacle

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
     void Start()
     {
         startingPosition = transform.position;
@@ -15,6 +16,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
